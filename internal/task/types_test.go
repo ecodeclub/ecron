ebcb5ce92c63b9d4b3aa1e37c3cb306c2f780603
jared.lu <jared-lu@outlook.com>
2024-07-11 23:36:48 +0800

    cron parse测试

---
+++ internal/task/types_test.go
@@ -0,0 +1,54 @@
+package task
+
+import (
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestName(t *testing.T) {
+	fmt.Println(int64(time.Second.Seconds()))
+}
+
+func TestTask_NextTime(t *testing.T) {
+	testCases := []struct {
+		name     string
+		cronExp  string
+		wantErr  error
+		interval time.Duration
+	}{
+		{
+			name:    "field解析成功",
+			cronExp: "0/5 * * * * ?",
+			wantErr: nil,
+		},
+		{
+			name:    "Descriptor解析成功",
+			cronExp: "@every 1s",
+			wantErr: nil,
+		},
+		{
+			name:    "field解析错误",
+			cronExp: "*/5 * * *",
+			wantErr: errors.New("expected exactly 6 fields"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			task := Task{
+				CronExp: tc.cronExp,
+			}
+			res, err := task.NextTime()
+			if err != nil {
+				assert.True(t, strings.Contains(err.Error(), tc.wantErr.Error()))
+				return
+			}
+			assert.Equal(t, tc.wantErr, err)
+			// 预期的下一次执行时间，会大于当前时间
+			assert.True(t, res.Sub(time.Now()).Seconds() > 0)
+		})
+	}
+}
